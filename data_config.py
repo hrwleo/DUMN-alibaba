091bd7e6f4504615bcb613da03f334b4ac1c266d
Stefan_hrw <812737452@qq.com>
2022-06-28 16:10:02 +0800

    Update data_config.py

--- data_config.py
+++ data_config.py
@@ -35,24 +35,8 @@ rank_dumn_config = {
 
 recall_config = {
     # user
-    "user_city_seq": tf.io.FixedLenFeature([5], tf.int64),
-    "user_shangquan_seq": tf.io.FixedLenFeature([5], tf.int64),
-    "user_comm_seq": tf.io.FixedLenFeature([5], tf.int64),
-    "user_price_seq": tf.io.FixedLenFeature([5], tf.int64),
-    "user_area_seq": tf.io.FixedLenFeature([5], tf.int64),
-
+    
     # prop
-    "city_id": tf.io.FixedLenFeature([1], tf.int64),
-    "comm_id": tf.io.FixedLenFeature([1], tf.int64),
-    "shangquan_id": tf.io.FixedLenFeature([1], tf.int64),
-    "price_id": tf.io.FixedLenFeature([1], tf.int64),
-    "area_id": tf.io.FixedLenFeature([1], tf.int64),
-    "floor_loc": tf.io.FixedLenFeature([1], tf.int64),
-    "total_floor": tf.io.FixedLenFeature([1], tf.int64),
-    "room_num": tf.io.FixedLenFeature([1], tf.int64),
-    "hall": tf.io.FixedLenFeature([1], tf.int64),
-    "bathroom": tf.io.FixedLenFeature([1], tf.int64),
-    "prop_age": tf.io.FixedLenFeature([1], tf.float32),
 
     # label
     "is_click": tf.io.FixedLenFeature([], tf.int64),
